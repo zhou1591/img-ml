c3d63d6f4b7c406de6492a8c04fe4c8f7cc8d99a
zhoujingsong <1402663440@qq.com>
2023-05-22 15:11:58 +0800

    l级别 简单图片识别

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from src.detection import Detector
+
+app = Flask(__name__)
+
+# Define the inference fn() for your models
+def model_inference(image):
+    image, json_out = Detector('PP-YOLOE+')(image)
+    return image, json_out
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the image from the request
+    image = request.files['image'].read()
+
+    # Run the inference
+    image, json_out = model_inference(image)
+
+    # Return the results as JSON
+    return jsonify(json_out)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
